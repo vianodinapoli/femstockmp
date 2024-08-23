065fa199dfe58e2f91be642400b30c6093c365d8
Viano Di Napoli <vianodinapoli@gmail.com>
2024-08-23 22:40:44 +0200

    conseguimos pegas os dados apartir do arquivo Json

--- components/table.js
+++ components/table.js
@@ -1,5 +1,5 @@
 import { BiEdit, BiTrashAlt } from "react-icons/bi";
-
+import data from '../database/data.json'
 export default function Table() {
   return (
     <table className="min-w-full table-auto">
@@ -32,46 +32,55 @@ export default function Table() {
         </tr>
       </thead>
       <tbody className="bg-gray-200">
-        <tr className="bg-gray-50 text-center">
-          <td className="px-16 py-2 flex flex-row item-center">
-            <img src="#" alt="" />
-            <span className="text-center ml-2 font-semibold">Ex.Ancosc</span>
-          </td>
+      {
+        data.map((obj,i) => <Tr{...obj} key={i}/>)
+      }
 
-          <td className="px-12 py-2">
-            <span>Anco</span>
-          </td>
-          <td className="px-12 py-2">
-            <span>2030</span>
-          </td>
-          <td className="px-12 py-2">
-            <span>2031</span>
-          </td>
-          <td className="px-12 py-2">
-            <span>2103</span>
-          </td>
-          <td className="px-12 py-2">
-            <span>SACO</span>
-          </td>
-          <td className="px-12 py-2">
-            <button className="cursor">
-              {" "}
-              <span className="bg-green-500 text-white px-5 py-1 rounded-full">
-                Activo
-              </span>
-            </button>
-          </td>
-
-          <td className="px-12 py-2 flex justify-around gap-5">
-            <button className="cursor">
-              <BiEdit size={25} color={"rgb(34,197,94)"}></BiEdit>
-            </button>
-            <button className="cursor">
-              <BiTrashAlt size={25} color={"rgb(244,63,94)"}></BiTrashAlt>
-            </button>
-          </td>
-        </tr>
       </tbody>
     </table>
   );
+}
+
+function Tr({referencia, designacao, fisico, primavera, armazem, unidade, status}){
+  return (
+    <tr className="bg-gray-50 text-center">
+    <td className="px-16 py-2 flex flex-row item-center">
+      <img src="#" alt="" />
+      <span className="text-center ml-2 font-semibold">{referencia || "N/A"}</span>
+    </td>
+
+    <td className="px-12 py-2">
+      <span>{designacao || "N/A"}</span>
+    </td>
+    <td className="px-12 py-2">
+      <span>{fisico || "N/A"}</span>
+    </td>
+    <td className="px-12 py-2">
+      <span>{primavera || "N/A"}</span>
+    </td>
+    <td className="px-12 py-2">
+      <span>{armazem || "N/A"}</span>
+    </td>
+    <td className="px-12 py-2">
+      <span>{unidade || "N/A"}</span>
+    </td>
+    <td className="px-12 py-2">
+      <button className="cursor">
+        {" "}
+        <span className="bg-green-500 text-white px-5 py-1 rounded-full">
+        {status || "N/A"}
+        </span>
+      </button>
+    </td>
+
+    <td className="px-12 py-2 flex justify-around gap-5">
+      <button className="cursor">
+        <BiEdit size={25} color={"rgb(34,197,94)"}></BiEdit>
+      </button>
+      <button className="cursor">
+        <BiTrashAlt size={25} color={"rgb(244,63,94)"}></BiTrashAlt>
+      </button>
+    </td>
+  </tr>
+  )
 }
