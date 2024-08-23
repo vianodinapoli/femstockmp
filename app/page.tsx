d361ea2f3d76745edc727899bfefe95d7a18a293
Viano Di Napoli <vianodinapoli@gmail.com>
2024-08-23 22:11:36 +0200

    criamos a ideia de usar o mesmo form para adicionar a ctualizar o form

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,20 @@
-import { BiPlus } from "react-icons/bi";
+'use client'
+import { BiPlus, BiUserPlus } from "react-icons/bi";
 import Table from '../components/table';
 import Head from 'next/head';
 import Form from '../components/form';
+import { useState } from "react";
 
 
 export default function Home() {
+
+const [visible, setVisible] = useState(false)
+const handler = () =>{
+  setVisible(visible ? false: true)
+  // ou podes   setVisible(!visible) isto na hora de clicar no botão e voltar a clicar para não vermos no front.
+
+}
+
   return (
     <>
       <Head>
@@ -20,7 +30,7 @@ export default function Home() {
 
         <div className="container mx-auto flex justify-between py-10 border-b">
           <div className="flex gap-4">
-            <button className='flex bg-indigo-500 text-white px-4 py-2 border rounded-md hover:bg-gray-500 hover:border-indigo-500 hover:text-gray-800'>
+            <button onClick={handler} className='flex bg-indigo-500 text-white px-4 py-2 border rounded-md hover:bg-gray-500 hover:border-indigo-500 hover:text-gray-800'>
               <BiPlus size={23} />
               <span className='px-1'></span>
               Adicionar artigo
@@ -31,9 +41,7 @@ export default function Home() {
 
         {/* formulario */}
 
-        <div className="container mx-auto py-3">
-        <Form></Form>
-        </div>
+        {visible ? <Form></Form>: <></>}
         {/* Seção para tabela */}
         <div className="container mx-auto">
           <Table></Table>
---
+++ components/updateUserForm.js
@@ -0,0 +1,123 @@
+"use client";
+import { BiBrush } from "react-icons/bi";
+import { useReducer } from "react";
+import Success from '../components/success';
+import Bug from '../components/bug';
+
+export default function UpdateUserForm() {
+  const formReducer = (state, event) => {
+    return {
+      ...state,
+      [event.target.name]: event.target.value,
+    };
+  };
+
+  const [formData, setFormData] = useReducer(formReducer, {});
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // Ao tentar adicionar o forma vazio: se não tivermos nada no formulário formData, retorna este erro!
+
+    if(Object.keys(formData).length == 0) return console.log("Por favor, preencha os dados de forma correcta!")
+    console.log(formData); 
+  };
+
+  if(Object.keys(formData).length > 0) return <Success message={"Artigo adicionado!"}/>;
+    // if (Object.keys(formData).length == 0) return <Bug message ={ Erro!}/>;
+
+  return (
+    <form className="grid lg:grid-cols-3 w-6/6 gap-4" onSubmit={handleSubmit}>
+      <div className="input-type">
+        <input
+          type="text"
+          onChange={setFormData}
+          name="referencia"
+          className="border w-full px-5 py-3 focus:outline-none rounded-md"
+          placeholder="Referência"
+        />
+      </div>
+
+      <div className="input-type">
+        <input
+          type="text"
+          onChange={setFormData}
+          name="designacao"
+          className="border w-full px-5 py-3 focus:outline-none rounded-md"
+          placeholder="Designação"
+        />
+      </div>
+
+      <div className="input-type">
+        <input
+          type="text"
+          onChange={setFormData}
+          name="fisico"
+          className="border w-full px-5 py-3 focus:outline-none rounded-md"
+          placeholder="Físico"
+        />
+      </div>
+
+      <div className="input-type">
+        <input
+          type="text"
+          onChange={setFormData}
+          name="primavera"
+          className="border w-full px-5 py-3 focus:outline-none rounded-md"
+          placeholder="Primavera"
+        />
+      </div>
+
+      <div className="input-type">
+        <input
+          type="text"
+          onChange={setFormData}
+          name="aramzem"
+          className="border w-full px-5 py-3 focus:outline-none rounded-md"
+          placeholder="Armazém"
+        />
+      </div>
+
+      <div className="input-type">
+        <input
+          type="text"
+          onChange={setFormData}
+          name="unidade"
+          className="border w-full px-5 py-3 focus:outline-none rounded-md"
+          placeholder="Unidade"
+        />
+      </div>
+
+      <div className="form-check">
+        <input
+          type="radio"
+          onChange={setFormData}
+          value="Active"
+          id="radioDefault1"
+          name="status"
+          className="form-check-input appearance-none rounded-full h-4 w-4 border border-gray-300 bg-white checked:bg-green-500 checked:border-green-500 focus:outline-none transition duration-200 mt-1 align-top bg-no-repeat bg-center bg-contain float-left mr-2 cursor-pointer"
+        />
+        <label htmlFor="radioDefault1" className="inline-block text-gray-800">
+          Active
+        </label>
+      </div>
+
+      <div className="form-check">
+        <input
+          type="radio"
+          onChange={setFormData}
+          value="Inactive"
+          id="radioDefault2"
+          name="status"
+          className="form-check-input appearance-none rounded-full h-4 w-4 border border-gray-300 bg-white checked:bg-green-500 checked:border-green-500 focus:outline-none transition duration-200 mt-1 align-top bg-no-repeat bg-center bg-contain float-left mr-2 cursor-pointer"
+        />
+        <label htmlFor="radioDefault2" className="inline-block text-gray-800">
+          Inactive
+        </label>
+      </div>
+      <hr />
+      <button className="flex justify-center text-md w-2/6 bg-blue-500 text-white px-4 py-2 border rounded-md hover:bg-gray-50 hover:border-green-500 hover:text-green-500">
+        <span className="px-1"><BiBrush size={25}></BiBrush></span>Actualizar
+      </button>
+    </form>
+  );
+}
